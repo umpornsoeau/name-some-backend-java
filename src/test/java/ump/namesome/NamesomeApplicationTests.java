a8d5e79cf7b87a9009e9b374faff7dd8368cbd55
umpornsoeau <umpornsoeau@protonmail.com>
2020-11-12 13:57:47 +0000

    init

---
+++ src/test/java/ump/namesome/NamesomeApplicationTests.java
@@ -0,0 +1,13 @@
+package ump.namesome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamesomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
